be197a861375fd2e1980ca6c39fed1be36f60ba4
Akhil M D <42731608+MDakhil1999@users.noreply.github.com>
2020-11-13 00:07:03 +0530

    12/11/2020

--- MyFirst.java
+++ MyFirst.java
@@ -3,5 +3,7 @@ class MyFirst
     public static void main(String args[])
     {
         System.out.println("Hello World!!");
+        System.out.println(args[0]);
+        System.out.println(args[1]);
     }
 }
